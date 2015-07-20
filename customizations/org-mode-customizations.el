880533784982b39a642b9e85e5d2291423110292
Roger Grantham <roger.grantham@pearson.com>
2015-07-20 11:25:27 -0500

    modifying todo list locations

--- customizations/org-mode-customizations.el
+++ customizations/org-mode-customizations.el
@@ -6,12 +6,12 @@
 (add-hook 'org-mode-hook 'flyspell-mode)
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
 (setq org-log-done t)
-(setq org-agenda-files (list "~/design/commonplace/diary.org"
-                             "~/design/commonplace/diary/01_2015.org"
-                             "~/design/commonplace/diary/02_2015.org"
-                             "~/design/commonplace/diary/03_2015.org"
-                             "~/design/commonplace/diary/04_2015.org"
-                             "~/design/commonplace/diary/05_2015.org"
+(setq org-agenda-files (list "~/commonplace/diary.org"
+                             "~/commonplace/diary/01_2015.org"
+                             "~/commonplace/diary/02_2015.org"
+                             "~/commonplace/diary/03_2015.org"
+                             "~/commonplace/diary/04_2015.org"
+                             "~/commonplace/diary/05_2015.org"
 ))
 (add-hook 'org-mode-hook 'turn-on-visual-line-mode)
 ;; Flyspell
